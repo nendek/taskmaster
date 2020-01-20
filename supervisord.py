52c9876ab1c9f37340411b20f63ad1e53ababe40
nendek <p.nardozi@gmail.com>
2020-01-20 09:56:50 -0500

    add server

--- supervisord.py
+++ supervisord.py
@@ -5,6 +5,8 @@ import signal
 import time
 from process import Process
 from orchestrator import Orchestrator
+import threading
+import socket
         
 class Supervisord:
     def __init__(self, conf_file):
@@ -17,15 +19,30 @@ class Supervisord:
         del self.claudio_abbado
         sys.exit(0)
 
-    def run(self):
-        self.claudio_abbado.show_processes()
+    def run_supervisord(self):
         while (1):
             self.claudio_abbado.update_processes()
 
+    def run_server(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(('', 5678))
+        self.socket.listen(5)
+        print('attend connexion')
+        stream_client, info_client = self.socket.accept()
+        print('connexion accept')
+        msg = b''
+        while msg != b'quit':
+            msg = stream_client.recv(1024)
+            print(msg)
+            stream_client.send(b'nude')
+        stream_client.close()
+        self.socket.close()
+
 def main(conf_file):
     supervisord = Supervisord(conf_file)
-    supervisord.run()
-
+    thread = threading.Thread(target=supervisord.run_supervisord, daemon=True)
+    thread.start()
+    supervisord.run_server()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
